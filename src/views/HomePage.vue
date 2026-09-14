5754601f79c5a05ee73330ac93c2d792e1ad95d5
Clifford <punzalan.clifford@minsu.edu.ph>
2026-09-14 08:06:18 +0800

    Remove local photo storage

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -13,27 +13,14 @@
 </template>
 
 <script setup lang="ts">
-import { onMounted, ref } from 'vue';
+import { ref } from 'vue';
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
 import CameraComponent from '../components/CameraComponent.vue';
 import PhotoGalleryComponent from '../components/PhotoGalleryComponent.vue';
 
 const photos = ref<string[]>([]);
-const storageKey = 'photo-gallery-photos';
-
-onMounted(() => {
-  const savedPhotos = localStorage.getItem(storageKey);
-  if (!savedPhotos) return;
-
-  try {
-    photos.value = JSON.parse(savedPhotos) as string[];
-  } catch {
-    localStorage.removeItem(storageKey);
-  }
-});
 
 function addPhoto(photo: string) {
   photos.value.unshift(photo);
-  localStorage.setItem(storageKey, JSON.stringify(photos.value));
 }
 </script>
